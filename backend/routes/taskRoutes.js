3cd04801084151f9b0cf8f6f39ef34b0a3075175
Miguel Angel Condori <miguelcondoriwtf2@gmail.com>
2026-06-10 00:22:55 -0400

    implementacion del auth en el crud de task

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -8,9 +8,9 @@ const router = express.Router();
 const {getTasks, createTask, updateTask, deleteTask} = require("../controllers/taskController");
 
 router.get("/", authMiddleware, getTasks);
-router.post("/", createTask);
-router.put("/:id", updateTask);
-router.delete("/:id", deleteTask)
+router.post("/", authMiddleware, createTask);
+router.put("/:id", authMiddleware, updateTask);
+router.delete("/:id", authMiddleware, deleteTask);
 
 
 module.exports = router;
