8f6c61f66b0cc1c1cf8fab5042e7cca78d9f478e
Miguel Angel Condori <miguelcondoriwtf2@gmail.com>
2026-06-22 21:36:44 -0400

    integracion del https con certificados en la parte del backend y del fronted

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
 
+
 const express = require("express");
 const cors = require("cors");
+
+const https = require("https");
+const fs = require("fs");
 
 const taskRoutes = require("./routes/taskRoutes");
 const authRoutes = require("./routes/authRoutes");
@@ -9,6 +13,12 @@ const todoListRoutes = require("./routes/todoListRoutes");
 const fileRoutes = require("./routes/fileRoutes");
 
 const app = express();
+
+const opciones = {
+  key: fs.readFileSync("key.pem"),
+  cert: fs.readFileSync("cert.pem")
+};
+
 
 app.use(cors());
 app.use(express.json());
@@ -19,9 +29,13 @@ app.use("/tasks", taskRoutes);
 app.use("/auth", authRoutes);
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hola estoy probando" });
+  res.json({ message: "Hola estoy probando, mensaje para verificar" });
 });
 
-app.listen(3000, () => {
-  console.log("hola desde el servidor");
+// app.listen(3000, () => {
+//   console.log("hola desde el servidor");
+// });
+
+https.createServer(opciones, app).listen(3000, () => {
+  console.log("Holi desde HTTPS verificar");
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 
 import axios from "axios";
 
-const API_URL = "http://localhost:3000";
+const API_URL = "https://localhost:3000";
 
 function App(){
   const [email, setEmail] = useState("");
